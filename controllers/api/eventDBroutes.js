2de011f79fd7d3b66eea50c2aa4ffe40723a17b9
Philip Garip <phil.garip@gmail.com>
2021-05-26 14:18:39 -0400

    started event routes

---
+++ controllers/api/eventDBroutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const { eq } = require('sequelize/types/lib/operators');
+const { Event } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const dbEventData = await Event.create({
+            title: req.body.title,
+            description: req.body.description,
+            time: req.body.time,
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(dbEventData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
