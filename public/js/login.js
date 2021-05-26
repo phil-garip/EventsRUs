3c015fd75be4f4ae62714f0116c4d386fd925251
Hawa <hawa.francis@gmail.com>
2021-05-26 13:23:19 -0400

    create profile

--- public/js/login.js
+++ public/js/login.js
@@ -1,15 +1,15 @@
 async function loginFormHandler(event) {
     event.preventDefault();
   
-    const username = document.querySelector('#email-login').value.trim();
+    const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    if (username && password) {
+    if (email && password) {
       const response = await fetch('/api/users/login', {
         method: 'post',
         body: JSON.stringify({
           email,
-          password
+          password,
         }),
         headers: { 'Content-Type': 'application/json' }
       });
@@ -23,4 +23,4 @@ async function loginFormHandler(event) {
   }
   
 
-document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
