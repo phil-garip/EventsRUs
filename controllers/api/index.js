1649a51a1a793f8bb56ac6a28d029f5618ce43a5
Hawa <hawa.francis@gmail.com>
2021-05-26 11:09:10 -0400

    dashboard handlebar

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,8 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 
-const dashboardRoute = require('./dashboard-route')
 
 router.use('/users', userRoutes);
-router.use('/dashboard', dashboardRoute)
+
 
 module.exports = router;
