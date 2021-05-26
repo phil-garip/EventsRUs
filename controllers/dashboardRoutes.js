e1f1b2b9feea4a653c609d8a1d9289d4da85d924
Philip Garip <phil.garip@gmail.com>
2021-05-26 11:11:41 -0400

    dashboard routes added

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -4,9 +4,3 @@ const { Event, User, Location } = require('../models');
 
 
 //GET all events
-router.get('/', async (req, res) => {
-    try {
-        const eventData = await Event.findAll();
-        res.status(200).json(eventData);
-    }
-})
