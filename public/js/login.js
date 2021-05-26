704a89c161e4b5d44f33da02d1f8c1d10e9d0e55
Danny Sollomoni <dsollomoni@gmail.com>
2021-05-26 14:21:48 -0400

    WIP

--- public/js/login.js
+++ public/js/login.js
@@ -1,26 +1,25 @@
-async function loginFormHandler(event) {
+const loginFormHandler = async (event) => {
+
     event.preventDefault();
   
-    const username = document.querySelector('#email-login').value.trim();
+    const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    if (username && password) {
+    if (email && password) {
       const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
         document.location.replace('/dashboard');
       } else {
-        alert(result.message);
+        alert('Failed to log in');
       }
     }
-  }
+  };
   
-
-document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
