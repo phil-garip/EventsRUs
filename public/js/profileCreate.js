eb40284bdd3cc34663220a693cc10b4815e317f7
Philip Garip <phil.garip@gmail.com>
2021-05-27 19:19:48 -0400

    profile create javasccript update

--- public/js/profileCreate.js
+++ public/js/profileCreate.js
@@ -2,12 +2,9 @@ const loginFormHandler = async (event) => {
 
     event.preventDefault();
   
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-    const firstName = document.querySelector('#first_name').value.trim();
-    const lastName = document.querySelector('#last_name').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
     const username = document.querySelector('#username').value.trim();
-    const birthday = document.querySelector('#birthday').value.trim();
   
     if (email && password && firstName && lastName && username && birthday) {
       const response = await fetch('/api/users/', {
